7e5f555712e050bef7efdfc3ba29b2c91b13b63e
raiamankumar <158134374+raiamankumar@users.noreply.github.com>
2026-01-01 23:06:06 +0530

    updated successfully

--- Tree/validbinarysearchtree.cpp
+++
@@ -1,9 +0,0 @@
-bool isValid(TreeNode* root, long min, long max) {
-    if (!root) return true;
-
-    if (root->val <= min || root->val >= max)
-        return false;
-
-    return isValid(root->left, min, root->val) &&
-           isValid(root->right, root->val, max);
-}
